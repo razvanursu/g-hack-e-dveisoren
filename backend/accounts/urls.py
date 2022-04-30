0be481a23eb292e8b2e596ab0c65e3e510bd4c19
Pouya Mostashar <mostashar@tufast.de>
2022-04-30 15:03:12 +0200

    loggin

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -4,4 +4,5 @@ from .views import SignUpView
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
+    #path('loggedin', SignUpView.as_view(template_name='start.html'), name='loggedin'),
 ]
