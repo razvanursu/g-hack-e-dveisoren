1df228093c0cb8b047a8616d85748cf7714d4f05
Razvan-Mihai Ursu <razvan.ursu@tum.de>
2022-04-30 17:48:36 +0200

    working users

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 
-from .views import SignUpView
+from . import views
 
 urlpatterns = [
-    path("signup/", SignUpView.as_view(), name="signup"),
-    
+    path("signup/", views.register_request, name="signup"),
 ]
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
-from django.views import generic
+from django.shortcuts import render, redirect
+from .forms import NewUserForm
+from django.contrib.auth import login
+from django.contrib import messages
 
 
-class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy("login")
-    template_name = "registration/signup.html"
+def register_request(request):
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful.")
+            return redirect("home")
+    messages.error(request, "Unsuccessful registration. Invalid information.")
+    form = NewUserForm()
+    return render(request=request, template_name="registration/signup.html", context={"register_form": form})
