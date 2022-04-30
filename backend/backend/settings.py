dd0ca186257f4ef8dea1ef3d2325480257373dac
Razvan-Mihai Ursu <razvan.ursu@tum.de>
2022-04-30 15:37:01 +0200

    update settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
     'default': {
         # If you are using Cloud SQL for MySQL rather than PostgreSQL, set
         # 'ENGINE': 'django.db.backends.mysql' instead of the following.
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.getenv('DATABASE_NAME'),
         'USER': os.getenv('DATABASE_USER'),
         'PASSWORD': os.getenv('DATABASE_PASSWORD'),
